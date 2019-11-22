d8e4793c913316d842491161b817c1c3a11f72a7
danielgon__ <daniel.goncalves@proaire.com.br>
2019-11-22 10:36:21 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import { create } from 'apisauce';
+
+const api = create({
+    baseURL: 'https://localhost:44351'
+});
+
+export default api;
